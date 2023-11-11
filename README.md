0c5ab12bcb2d35022d407525ca9e464cf6da99f9
tyler <tylermontobbio@gmail.com>
2023-11-10 16:58:27 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# React + Vite
+
+```
+//get started
+npm i
+
+//development server command
+npm run dev
+```
+
+### Added Dependencies
+
+react-scroll
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Nav from './nav/Nav.jsx'
+import Home from "./home/Home.jsx"
+import About from "./about/About.jsx"
+import Projects from "./projects/Projects.jsx"
+import Contact from "./contact/Contact.jsx"
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+    </>
+  )
+}
+
+export default App
